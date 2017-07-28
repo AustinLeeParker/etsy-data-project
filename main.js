9115ec08503f5a6a60983fce6e62f9c33c68598d
AustinLeeParker <tehdemopan@gmail.com>
2017-07-28 10:05:42 -0500

    Created array and an average function

--- main.js
+++ main.js
@@ -7,40 +7,54 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
+function question1() {
   // Answer:
+  var total = 0;
+  for (i = 0; i < data.length; i++) {
+    total = data[i].price + total;
+
+  }
+  console.log(total / data.length)
+  return total / data.length;
 }
 
 
-
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
+function question2() {
   // Answer:
+  var items = [];
+  for (i = 0; i < data.length; i++) {
+    if (data[i].price >= 14.00 && data[i].price <= 18.00) {
+      items.push(data[i]);
+    }
+  }
+  console.log(items);
+  return items;
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
   // Answer:
 }
 
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+function question4() {
   // Answer:
 }
 
 
 // 5: Which items are made of eight or more materials? 
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
 }
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
 }
